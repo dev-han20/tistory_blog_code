dad5fa475fee34aa6152ee558515a845ca65e159
CodingNoJam <hanwoodww@gmail.com>
2021-06-02 23:11:17 +0900

    solving

--- src/study/blog/codingnojam/algorithm/BOJ_2042.java
+++ src/study/blog/codingnojam/algorithm/BOJ_2042.java
@@ -1,19 +1,23 @@
 package study.blog.codingnojam.algorithm;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class BOJ_2042 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        String[] info = br.readLine().split(" ");
+        // 0인덱스는 사용안하기 위해 배열길이 1 추가
+        int[] arr = new int[Integer.parseInt(info[0])+1];
+        for (int i = 1; i <= Integer.parseInt(info[0]); i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+    }
 
-
+    public static void segmentTreeInit(){
 
     }
 
