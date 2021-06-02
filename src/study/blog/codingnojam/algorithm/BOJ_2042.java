8852fc44b94656a8142043f0fe0bdd66d72fda51
CodingNoJam <hanwoodww@gmail.com>
2021-06-03 00:05:57 +0900

    solving

--- src/study/blog/codingnojam/algorithm/BOJ_2042.java
+++ src/study/blog/codingnojam/algorithm/BOJ_2042.java
@@ -5,9 +5,6 @@ import java.io.*;
 public class BOJ_2042 {
 
     public static void main(String[] args) throws IOException {
-
-        segmentTreeInit(16);
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -18,15 +15,28 @@ public class BOJ_2042 {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
+    }
+
+    static class SegmentTree{
+        private double treeHeight;
+        private long treeNodeCount;
+
+        public SegmentTree(int arrLength) {
+            treeHeight  = Math.log(arrLength)/Math.log(2)+1;
+            treeNodeCount = Math.round(Math.pow(2, treeHeight)-1);
+        }
 
     }
 
-    public static void segmentTreeInit(int arrLength){
+    public static void segmentTreeCreate(int arrLength){
         double treeHeight = Math.log(arrLength)/Math.log(2)+1;
         long treeNodeCount = Math.round(Math.pow(2, treeHeight)-1);
 
-        System.out.println("treeHeight = " + treeHeight);
-        System.out.println("treeNodeCount = " + treeNodeCount);
+
+//        System.out.println("treeHeight = " + treeHeight);
+//        System.out.println("treeNodeCount = " + treeNodeCount);
+
+
 
     }
 
