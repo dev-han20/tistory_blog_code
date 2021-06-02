15b7193fa0dbadbef853b8a12837efd78474043c
CodingNoJam <hanwoodww@gmail.com>
2021-06-02 22:42:23 +0900

    solving

--- src/study/blog/codingnojam/algorithm/BOJ_2042.java
+++ src/study/blog/codingnojam/algorithm/BOJ_2042.java
@@ -1,14 +1,18 @@
 package study.blog.codingnojam.algorithm;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class BOJ_2042 {
 
     public static void main(String[] args) {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
 
 
     }
