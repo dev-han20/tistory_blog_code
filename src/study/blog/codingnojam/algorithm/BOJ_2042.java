8d13eaa367f0a620c868c5559c223f2978b8c16b
CodingNoJam <hanwoodww@gmail.com>
2021-06-06 22:27:24 +0900

    solving

--- src/study/blog/codingnojam/algorithm/BOJ_2042.java
+++ src/study/blog/codingnojam/algorithm/BOJ_2042.java
@@ -1,6 +1,8 @@
 package study.blog.codingnojam.algorithm;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BOJ_2042 {
 
@@ -10,21 +12,34 @@ public class BOJ_2042 {
 
         String[] info = br.readLine().split(" ");
         // 0인덱스는 사용안하기 위해 배열길이 1 추가
-        int[] arr = new int[Integer.parseInt(info[0])+1];
+        long[] arr = new long[Integer.parseInt(info[0])+1];
         for (int i = 1; i <= Integer.parseInt(info[0]); i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            arr[i] = Long.parseLong(br.readLine());
         }
 
-        SegmentTree st = new SegmentTree(10);
+        SegmentTree st = new SegmentTree(arr.length);
+
+
+
+
     }
 
     static class SegmentTree{
         private long treeNodeCount;
+        private long[] tree;
 
-        // 생성자에서 트리의 전체노드개수 계산
+        // 생성자에서 세그먼트트리의 전체노드개수 계산
         SegmentTree(int arrLength) {
             double treeHeight = Math.log(arrLength)/Math.log(2)+1;
             treeNodeCount = Math.round(Math.pow(2, treeHeight)-1);
+            tree = new long[(int)treeNodeCount + 1];
+        }
+
+        // 트리 생성을 위한 초기화 메서드
+        void init(long[] arr, int node, int start, int end ){
+            if (start == end) {
+                tree[node]
+            }
         }
 
 
