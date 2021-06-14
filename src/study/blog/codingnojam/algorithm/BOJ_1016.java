a83b84a30a5a135079ce51d4e3dad8997dbe5d21
CodingNoJam <hanwoodww@gmail.com>
2021-06-14 20:59:27 +0900

    solving

---
+++ src/study/blog/codingnojam/algorithm/BOJ_1016.java
@@ -0,0 +1,26 @@
+package study.blog.codingnojam.algorithm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_1016 {
+
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] info = br.readLine().split(" ");
+        long min = Long.parseLong(info[0]);
+        long max = Long.parseLong(info[1]);
+
+        for (int i = 2; i <= 1000000; i++) {
+
+        }
+
+
+
+
+    }
+
+}
