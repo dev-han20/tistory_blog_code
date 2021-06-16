5b18a56d037fe437908c085383a6bc3393ea81a1
CodingNoJam <hanwoodww@gmail.com>
2021-06-16 22:40:41 +0900

    commecnt writing

--- src/study/blog/codingnojam/algorithm/BOJ_1016.java
+++ src/study/blog/codingnojam/algorithm/BOJ_1016.java
@@ -21,7 +21,7 @@ public class BOJ_1016 {
         // 배열에 저장 될 값 초기화를 위해 사용할 변수
         long init = min;
 
-        // 제곱수 개수 저장 할 변수
+        // 제곱ㄴㄴ 개수 저장 할 변수
         int count = 0;
 
         // 배열 초기화 (min ~ max까지의 값 배열에 저장)
@@ -31,28 +31,45 @@ public class BOJ_1016 {
                 break;
             }
         }
-        
+
+        // 1 이상의 수에서 제곱을 해야 하기때문에 2부터 시작
+        // 문제에서 max의 최대값은 1조1백만이므로 백만*백만 까지만 반복하면 됨
+        // 1000001 * 1000001은 1조1백만 이상의 값이 나옴
         for (long i = 2; i <= 1000000 ; i++) {
+            // 제곱수 계산
             long squareNumber = i * i;
+
+            // 제곱수가 max값 보다 크면 더 이상 반복하지 않음
             if (squareNumber > max) {
                 break;
-            } else {
-                long temp = (min / squareNumber);
-                if (temp * squareNumber < min) {
-                    temp++;
+            } else { // 제곱수가 max값 보다 작을 때
+
+                // 제곱수로 나누어떨어지는 가장 작은 값을 찾기 위한 연산 시작
+                // min을 제곱수로 나누어서 몫을 구함
+                long quot = (min / squareNumber);
+                // 구한 몫에 다시 제곱수를 곱해서 그 값이 min보다 작으면 몫에 1을 더함
+                if (quot * squareNumber < min) {
+                    quot++;
                 }
-                long j = (temp * squareNumber) - min;
+                // 계산된 몫에 제곱수를 곱한 후 min값을 뺴면 배열의 인덱스를 구할 수 있음
+                long j = (quot * squareNumber) - min;
+
+                // 배열에 위에서 구한 인덱스의 값을 0으로 변경 (제곱수로 나누어지는 숫자임)
+                // 인덱스에 제곱수만큼 더해서 그 다음 인덱스의 값을 0으로 변경 (제곱수의 배수이므로 나누어지는 수임)
+                // 배열의 길이보다 커질 때까지 반복
                 for (long k = j; k < arr.length; k = k+squareNumber) {
                     arr[(int)k] = 0;
                 }
             }
         }
+        // 배열에서 제곱 ㄴㄴ수 개수 세기
         for (int i = 0; i < arr.length ; i++) {
             if(arr[i] != 0){
                 count++;
             }
         }
 
+        // 출력
         System.out.println(count);
     }
 }
