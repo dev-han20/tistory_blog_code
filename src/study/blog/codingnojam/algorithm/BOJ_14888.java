1917594992808ba079e10050d5dc41ae3f2e4e39
CodingNoJam <hanwoodww@gmail.com>
2021-07-14 22:33:16 +0900

    sollving

--- src/study/blog/codingnojam/algorithm/BOJ_14888.java
+++ src/study/blog/codingnojam/algorithm/BOJ_14888.java
@@ -1,10 +1,18 @@
 package study.blog.codingnojam.algorithm;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 // 백준온라인저지 14888번 연산자 끼워넣기 문제풀이
 public class BOJ_14888 {
 
     public static void main(String[] args) {
-        
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        ttest
+
+
     }
 
 }
