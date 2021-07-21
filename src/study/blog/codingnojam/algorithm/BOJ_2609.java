7a8b019d895c8ffd41af414d4a3d6a02867417a3
CodingNoJam <hanwoodww@gmail.com>
2021-07-21 20:42:13 +0900

    no message

---
+++ src/study/blog/codingnojam/algorithm/BOJ_2609.java
@@ -0,0 +1,17 @@
+package study.blog.codingnojam.algorithm;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BOJ_2609 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if (Math.max())
+    }
+}
