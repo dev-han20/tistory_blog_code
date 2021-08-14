77d1dd8d8b9bc170b1223fe79936027a356323f8
CodingNoJam <hanwoodww@gmail.com>
2021-08-14 10:11:04 +0900

    solve

---
+++ src/study/blog/codingnojam/algorithm/programmers/Solution_예상대진표.java
@@ -0,0 +1,20 @@
+package study.blog.codingnojam.algorithm.programmers;
+
+// 2017 팁스타운 - 예상 대진표
+class Solution_예상대진표
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        a = a + n-1;
+        b = b + n-1;
+
+        while (a != b) {
+            a = a / 2;
+            b = b / 2;
+            answer++;
+        }
+        return answer;
+
+    }
+}
