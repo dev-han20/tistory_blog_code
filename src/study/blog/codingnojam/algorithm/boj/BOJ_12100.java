dd83b75fb97250842a4306db4e852450424198b1
CodingNoJam <hanwoodww@gmail.com>
2021-08-18 23:18:31 +0900

    solving

--- src/study/blog/codingnojam/algorithm/boj/BOJ_12100.java
+++ src/study/blog/codingnojam/algorithm/boj/BOJ_12100.java
@@ -5,27 +5,49 @@ import java.io.*;
 // 백준온라인저지 12100 2048(easy) 문제 풀이 Java
 public class BOJ_12100 {
 
+    static int[] moveR = {-1, 1, 0, 0};
+    static int[] moveC = {0, 0, -1, 1};
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[][] map = new int[n][n];
+        int[][] board = new int[n][n];
         boolean[][] sumChk = new boolean[n][n];
 
         for (int i = 0; i < n; i++) {
             String[] info = br.readLine().split(" ");
             for (int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(info[j]);
+                board[i][j] = Integer.parseInt(info[j]);
             }
         }
 
-        recursion(0, 5);
+        recursion(0, 5, board);
 
 
     }
 
-    static void recursion(int index, int limit) {
+    static void recursion(int index, int limit, int[][] board) {
+
+        for (int i = 0; i < 4; i++) {
+            if (i == 0) { //상
+                for (int j = 0; j < board.length; j++) {
+                    for (int k = 0; k < board.length; k++) {
+                        
+                    }
+                }
+                
+
+            } else if (i == 1) {
+
+            } else if (i == 2) {
+
+            } else {
+                
+            }
+            
+        }
 
     }
 
