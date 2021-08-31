6bde00d5400ff96edd0e7928f2c862575293c8e3
CodingNoJam <hanwoodww@gmail.com>
2021-08-31 23:36:43 +0900

    solving

--- src/study/blog/codingnojam/algorithm/codility/Solution.java
+++ src/study/blog/codingnojam/algorithm/codility/Solution.java
@@ -4,40 +4,32 @@ package study.blog.codingnojam.algorithm.codility;// you can also use imports, f
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
 
-import java.util.Arrays;
-
 class Solution {
+    public int solution(int[] A, int[] B) {
+        // write your code in Java SE 8
 
-    public static void main(String[] args) {
-        int[] ee = {8,8,4,3};
-        int k = 2;
-        System.out.println(solution(ee, k));
-    }
-
-    public static int solution(int[] A, int K) {
-
-        int result = Integer.MAX_VALUE;
-
-        for (int i = 0; i <= A.length-K; i++) {
-
-            int[] temp = new int[K];
-            int[] tempA = new int[A.length];
-            for (int j = 0; j < A.length; j++) {
-                tempA[j] = A[j];
-            }
-
-            for (int j = i; j < K+i; j++) {
-                temp[j - i] = tempA[j];
-                tempA[j] = -1;
-            }
-
-            Arrays.sort(tempA);
-            int minValue = tempA[K];
-            int maxValue = tempA[A.length - 1];
-            int sub = maxValue - minValue;
-            result = Math.min(result, sub);
+        if (A.length == 0) {
+            return 0;
         }
 
-        return result;
+        for (int i = 0; i < A.length-1; i++) {
+            int lt = i;
+            int rt = i+1;
+            int result = 1;
+            while (true) {
+                if (B[lt] < A[rt]) {
+
+                    result++;
+                    lt = rt;
+                    rt++;
+                } else {
+                    rt++;
+                }
+            }
+
+        }
+
+        return 1;
+
     }
 }
