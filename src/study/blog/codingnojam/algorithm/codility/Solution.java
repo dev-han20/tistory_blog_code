bbbcde953888d6b0f048f3e219f8ecc716805e72
CodingNoJam <hanwoodww@gmail.com>
2021-09-01 11:39:51 +0900

    solve

--- src/study/blog/codingnojam/algorithm/codility/Solution.java
+++ src/study/blog/codingnojam/algorithm/codility/Solution.java
@@ -1,35 +1,15 @@
-package study.blog.codingnojam.algorithm.codility;// you can also use imports, for example:
-// import java.util.*;
-
-// you can write to stdout for debugging purposes, e.g.
-// System.out.println("this is a debug message");
+package study.blog.codingnojam.algorithm.codility;
 
 class Solution {
-    public int solution(int[] A, int[] B) {
-        // write your code in Java SE 8
-
-        if (A.length == 0) {
-            return 0;
+    public boolean solution(int[] A, int K) {
+        int n = A.length;
+        for (int i = 0; i < n - 1; i++) {
+            if (A[i] + 1 < A[i + 1] && A[i]+1 <= K)
+                return false;
         }
-        int r = 0;
-
-        for (int i = 0; i < A.length-1; i++) {
-            int lt = i;
-            int rt = i+1;
-            int result = 1;
-            while (rt < A.length) {
-                if (B[lt] < A[rt]) {
-                    result++;
-                    lt = rt;
-                    rt++;
-                } else {
-                    rt++;
-                }
-            }
-            r = Math.max(r, result);
-
-        }
-        return r;
-
+        if (A[0] != 1 || A[n - 1] < K)
+            return false;
+        else
+            return true;
     }
 }
