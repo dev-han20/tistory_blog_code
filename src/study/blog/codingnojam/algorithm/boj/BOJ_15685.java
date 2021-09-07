617dc4f9ea103a5d3a26589141052ec31a59593d
CodingNoJam <hanwoodww@gmail.com>
2021-09-07 22:14:30 +0900

    solving

---
+++ src/study/blog/codingnojam/algorithm/boj/BOJ_15685.java
@@ -0,0 +1,15 @@
+package study.blog.codingnojam.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// 백준온라인저지 15685번 드래곤커브 문제풀이
+public class BOJ_15685 {
+
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
