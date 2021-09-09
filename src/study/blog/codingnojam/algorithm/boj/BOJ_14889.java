5cff102c5462614afa8228dae3624c79589c352b
CodingNoJam <hanwoodww@gmail.com>
2021-09-09 22:21:25 +0900

    solving

--- src/study/blog/codingnojam/algorithm/boj/BOJ_14889.java
+++ src/study/blog/codingnojam/algorithm/boj/BOJ_14889.java
@@ -1,9 +1,15 @@
 package study.blog.codingnojam.algorithm.boj;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 // 백준온라인저지 14889번 스타트와 링크 문제 풀이
 public class BOJ_14889 {
 
     public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 
     }
 }
