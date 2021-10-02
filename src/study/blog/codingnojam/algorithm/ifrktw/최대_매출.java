1e5758dbf0d8e9d0ecbd6943ad1e83e0c49b2e3e
CodingNoJam <hanwoodww@gmail.com>
2021-10-02 22:58:52 +0900

    solve

---
+++ src/study/blog/codingnojam/algorithm/ifrktw/최대_매출.java
@@ -0,0 +1,32 @@
+package study.blog.codingnojam.algorithm.ifrktw;
+
+import java.util.Scanner;
+
+public class 최대_매출 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i <N ; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int front = K;
+        int back = 0;
+        int result = 0;
+
+        for (int i = 0; i < K; i++) {
+            result += arr[i];
+        }
+        int sum = result;
+        while (front < arr.length) {
+            sum = sum - arr[back] + arr[front];
+            result = Math.max(result, sum);
+            back++;
+            front++;
+        }
+        System.out.println(result);
+    }
+}
