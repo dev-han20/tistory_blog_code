ab7fe04ca345f3359c65bca95ca148f373fd7428
CodingNoJam <hanwoodww@gmail.com>
2021-10-03 18:28:20 +0900

    solve

--- src/study/blog/codingnojam/algorithm/programmers/Solution.java
+++ src/study/blog/codingnojam/algorithm/programmers/Solution.java
@@ -1,15 +1,58 @@
 package study.blog.codingnojam.algorithm.programmers;
 
-class Solution {
-    public int solution(int n) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
-        int[] dp = new int[n + 1];
-        dp[2] = 3;
-        for (int i = 3; i < dp.length; i++) {
-            if (i % 2 == 0) {
-                dp[i] = (dp[i-2] * 3 + 2) % 1000000007;
-            }
+class Solution {
+
+    public static void main(String[] args) throws IOException {
+        Solution s = new Solution();
+
+        StringBuilder sb = new StringBuilder();
+        sb.toString();
+//        String q = " ABCDEF";
+        String q = " ABCDEFG";
+
+        ArrayList<String> list = new ArrayList<>();
+//        LinkedList<String> list = new LinkedList<>();
+
+        String[] tree = q.split("");
+        System.out.println(tree[3]);
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("ee", map.getOrDefault("ee",0)+1);
+//        int[][] mo = {{10, 4}, {300, 1}, {90, 4},{260, 4}};
+//        s.solution(mo);
+
+        DFS(1, tree, sb);
+        System.out.println(sb.toString());
+
+
+    }
+
+    public static void DFS(int index,String[] tree, StringBuilder sb) {
+        if ((index * 2) > tree.length) {
+            sb.append(tree[index]);
+            return;
         }
-        return dp[n];
+
+        DFS(index * 2, tree, sb);
+        if (index * 2 + 1 < tree.length) {
+            DFS(index * 2 + 1, tree, sb);
+        }
+        sb.append(tree[index]);
+    }
+
+    public int solution(int[][] money) {
+
+        Arrays.sort(money, (o1,o2) -> o2[0] - o1[0]);
+
+        System.out.println("---");
+        for (int[] ints : money) {
+            System.out.println(ints[0]);
+        }
+        return 1;
     }
 }
---
+++ src/study/blog/codingnojam/algorithm/programmers/위클리_챌린지_8주차_최소직사각형.java
@@ -0,0 +1,27 @@
+package study.blog.codingnojam.algorithm.programmers;
+
+public class 위클리_챌린지_8주차_최소직사각형 {
+    public static void main(String[] args) {
+    }
+
+    private class Solution {
+        public int solution(int[][] sizes) {
+            int wMax = 0;
+            int hMax = 0;
+            for(int i=0; i<sizes.length; i++){
+                int w = sizes[i][0];
+                int h = sizes[i][1];
+                if(w < h){
+                    sizes[i][0] = h;
+                    sizes[i][1] = w;
+                }
+
+                wMax = Math.max(sizes[i][0], wMax);
+                hMax = Math.max(sizes[i][1], hMax);
+            }
+
+            int answer = wMax * hMax;
+            return answer;
+        }
+    }
+}
