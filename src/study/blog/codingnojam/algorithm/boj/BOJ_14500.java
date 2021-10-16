c56bb778725caab87a0b82226bbdac694cc59bb6
HamNeck <hanwoodww@gmail.com>
2021-10-16 22:46:30 +0900

    solving

---
+++ src/study/blog/codingnojam/algorithm/boj/BOJ_14500.java
@@ -0,0 +1,30 @@
+package study.blog.codingnojam.algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_14500 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] NM = br.readLine().split(" ");
+        int N = Integer.parseInt(NM[0]);
+        int M = Integer.parseInt(NM[1]);
+        int[][] map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String[] tempString = br.readLine().split(" ");
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(tempString[j]);
+            }
+        }
+
+
+
+
+    }
+
+
+}
