0ba0e896471ab574482aed2cdac98abddad35037
HamNeck <hanwoodww@gmail.com>
2021-10-18 21:36:51 +0900

    solving

--- src/study/blog/codingnojam/algorithm/programmers/위클리챌린지_9주차_전력망을둘로나누기.java
+++ src/study/blog/codingnojam/algorithm/programmers/위클리챌린지_9주차_전력망을둘로나누기.java
@@ -14,18 +14,29 @@ public class 위클리챌린지_9주차_전력망을둘로나누기 {
         }
         for (int i = 0; i < wires.length; i++) {
             topList.get(wires[i][0]).connectTops.add(new Top(wires[i][1]));
+            topList.get(wires[i][1]).connectTops.add(new Top(wires[i][0]));
         }
 
-    int answer = -1;
+        for (int i = 0; i < wires.length; i++) {
+            for (int j = 0; j < topList.get(wires[i][0]).connectTops.size(); j++) {
+                if (topList.get(wires[i][0]).connectTops.get(j).index == wires[i][1]) {
+
+                }
+            }
+            topList.get(wires[i][1]).connectTops.add(new Top(wires[i][0]));
+        }
+
+
+        int answer = -1;
         return answer;
-}
-
-class Top {
-    int index;
-    ArrayList<Top> connectTops = new ArrayList<>();
-
-    Top(int index) {
-        this.index = index;
     }
-}
+
+    class Top {
+        int index;
+        ArrayList<Top> connectTops = new ArrayList<>();
+
+        Top(int index) {
+            this.index = index;
+        }
+    }
 }
