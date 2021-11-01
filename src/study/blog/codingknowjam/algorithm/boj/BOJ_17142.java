fa602d355854409b9a7549727fc211e7339503da
CodingNoJam <hanwoodww@gmail.com>
2021-11-01 23:46:21 +0900

    solving

--- src/study/blog/codingknowjam/algorithm/boj/BOJ_17142.java
+++ src/study/blog/codingknowjam/algorithm/boj/BOJ_17142.java
@@ -3,25 +3,31 @@ package study.blog.codingknowjam.algorithm.boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 // 백준온라인저지 17142번 연구소3 문제 풀이
 public class BOJ_17142 {
 
 	static int N;
 	static int M;
-	static int[][] arr;
+	static int[][] map;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] NM = br.readLine().split(" ");
 		N = Integer.parseInt(NM[0]);
 		M = Integer.parseInt(NM[1]);
-		arr = new int[N][N];
+		map = new int[N][N];
 
+		ArrayList<Virus> virusList = new ArrayList<>();
 		for (int i = 0; i < N; i++) {
 			String[] temp = br.readLine().split(" ");
 			for (int j = 0; j < temp.length; j++) {
-				arr[i][j] = Integer.parseInt(temp[j]);
+				int objectNumber = Integer.parseInt(temp[j]);
+				if (objectNumber == 2) {
+					virusList.add(new Virus(i, j));
+				}
+				map[i][j] = objectNumber;
 			}
 		}
 
@@ -30,5 +36,19 @@ public class BOJ_17142 {
 
 
 
+
+
+
 	}
+
+	static class Virus {
+		int row;
+		int col;
+
+		public Virus(int row, int col) {
+			this.row = row;
+			this.col = col;
+		}
+	}
+
 }
