e61be26f83f7681793234b6139c780b902644c4c
condingknowjam <hanwoodww@gmail.com>
2021-11-14 20:25:34 +0900

    docs: 벡준 17142 문제풀이 포스팅

--- src/study/blog/codingknowjam/algorithm/boj/BOJ_17142.java
+++ src/study/blog/codingknowjam/algorithm/boj/BOJ_17142.java
@@ -23,6 +23,7 @@ public class BOJ_17142 {
 	static TreeSet<Integer> result = new TreeSet<>();
 
 	public static void main(String[] args) throws IOException {
+		// 정보 초기화
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] NM = br.readLine().split(" ");
 		N = Integer.parseInt(NM[0]);
@@ -31,6 +32,7 @@ public class BOJ_17142 {
 		visited = new int[N][N];
 		target = new int[M];
 
+		// 바이러스 위치, 초기 빈칸개수 정보 저장 및 맵 초기화
 		for (int i = 0; i < N; i++) {
 			String[] temp = br.readLine().split(" ");
 			for (int j = 0; j < temp.length; j++) {
@@ -44,12 +46,17 @@ public class BOJ_17142 {
 				map[i][j] = objectNumber;
 			}
 		}
+
+		// 빈칸이 하나도 없으면 0초
 		if (blankCount == 0) {
 			System.out.println(0);
 			return;
 		}
 
+		// 주어진 바이러스에서 n개의 바이러스 조합 찾아서 BFS수행(모든 경우의 찾기)
 		DFS(0, 0);
+
+		//결과 출력
 		if (result.contains(-1)) {
 			if (result.size() == 1) {
 				System.out.println(-1);
@@ -62,7 +69,9 @@ public class BOJ_17142 {
 	}
 
 	private static void DFS(int lev, int index) {
+		// 하나의 조합이 완료된 경우
 		if (lev == M) {
+			// BFS수행을 위해 정보 초기홯
 			int caseResult = 0;
 			int caseBlankCount = blankCount;
 			for (int i = 0; i < N; i++) {
@@ -70,11 +79,14 @@ public class BOJ_17142 {
 					visited[i][j] = map[i][j];
 				}
 			}
+			// 큐에 바이러스 넣기
 			Queue<Virus> q = new LinkedList<>();
 			for (int i = 0; i < target.length; i++) {
 				Virus virus = virusList.get(target[i]);
 				q.offer(new Virus(virus.row, virus.col, virus.timeCount));
 			}
+
+			//BFS 수행
 			queueLoopOut:
 			while (!q.isEmpty()) {
 				Virus virus = q.poll();
@@ -102,16 +114,18 @@ public class BOJ_17142 {
 			return;
 		}
 
+		// 모든 경우의 수 조합 생성
 		for (int i = index; i < virusList.size(); i++) {
 			target[lev] = i;
 			DFS(lev + 1, i + 1);
 		}
 	}
 
+	// 바이러스 클래스
 	static class Virus {
 		int row;
 		int col;
-		int timeCount = 0;
+		int timeCount = 0;	// 몇초에 생성된 바이러스인지 체크를 위한 변수
 
 		public Virus(int row, int col, int timeCount) {
 			this.row = row;
